a79385f6210c35a72a3ac2a1bbf2c515ab0ce031
unknown <169146967+WillMacBaker@users.noreply.github.com>
2025-07-13 16:02:29 +0100

    separating tasks and items out into new pages

---
+++ script.js
@@ -0,0 +1,6 @@
+// This is a JavaScript file! If you've done more HTML and CSS work, you'll likely spend a bit more time working in here.
+// However, if you're totally new, feel free to ask me questions, and I'll do my best to help out! :) I'm still learning as well.
+
+
+
+// In modern web development, a lot of professional websites will usually utilise some form of front-end library. These libraries - however fancy - all tend 
